011d1577467beae410a4d61f24fa4cdbdc6e3501
Andhy Ralac <andhyralac11@gmail.com>
2022-08-24 00:27:40 -0600

    se agrega la validacion de crear usuario a la ruta post

--- src/components/user/routes.js
+++ src/components/user/routes.js
@@ -2,11 +2,11 @@ const { Router } = require('express')
 const router = Router()
 
 const userController = require('./controller')
-
+const userValidators = require('./validator/user.validators')
 
 
 //ruta post para la creacion de usuario
-router.post('/', userController.createUser)
+router.post('/', userValidators.validateUserObjectDataCreate, userController.createUser)
 
 
 
